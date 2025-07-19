35c8ee3788d43804e37dd374923fccad0d0dd5fc
mylesfuentes1 <myles.fuentes@csuglobal.edu>
2025-07-19 17:26:34 -0600

    Create Restaurantlist.java

---
+++ Restaurantlist.java
@@ -0,0 +1,15 @@
+package module1;
+
+public class Restaurantlist {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.print("Restaurant Name:");
+			System.out.println( "Snarf's");
+			System.out.print("Business Address:");
+			System.out.println("7495 N Academy Blvd");
+			System.out.print("City,State,Zip Code:");
+			System.out.println("Colorado Springs,CO,80920");
+			
+	}
+}
